3af7b11f6d57ff44f2dd0e875c7bb33cb888cec9
pymaniac <pymaniac@users.noreply.github.com>
2026-06-10 02:44:05 +0000

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# meta-edge-ai/setup.sh
+MACHINE=imx8mq-evk DISTRO=fsl-imx-wayland source ./imx-setup-release.sh -b build
+cat sources/meta-edge-ai/conf/local.conf.append >> build/conf/local.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-edge-ai\"" >> build/conf/bblayers.conf
