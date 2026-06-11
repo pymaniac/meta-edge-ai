1a50dc6fe435bde8d9f0e1823883e0dd9eaf33ae
pymaniac <pymaniac@users.noreply.github.com>
2026-06-11 17:09:24 +0000

    Fix script and regdb errors on boot
    Signed-off-by: pymaniac <pymaniac@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # meta-edge-ai/setup.sh
 MACHINE=imx8mq-evk DISTRO=fsl-imx-wayland source ./imx-setup-release.sh -b build
-cat sources/meta-edge-ai/conf/local.conf.append >> build/conf/local.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-edge-ai\"" >> build/conf/bblayers.conf
+cat ../sources/meta-edge-ai/conf/local.conf.append >> conf/local.conf
+sed -i 's/imx8mqevk/imx8mq-evk/g' conf/local.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-edge-ai\"" >> conf/bblayers.conf
